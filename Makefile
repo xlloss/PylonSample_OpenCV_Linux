ea85d7f9dcc6a24c34689e6ddd18ac2c75e753c5
Slash.Huang <slash.huang@dfi.com>
2020-12-21 16:51:58 +0800

    code clean up in my environment

--- Makefile
+++ Makefile
@@ -5,14 +5,14 @@
 NAME       := openCVGrab
 
 # Installation directories for pylon
-PYLON_ROOT ?= /opt/pylon5
+PYLON_ROOT ?= /opt/pylon
 
 # Build tools and flags
 LD         := $(CXX)
-CPPFLAGS   := $(shell $(PYLON_ROOT)/bin/pylon-config --cflags)
+CPPFLAGS   := $(shell $(PYLON_ROOT)/bin/pylon-config --cflags) -I/media/slash/500G/opencv/installation/OpenCV-3.4.8/include
 CXXFLAGS   := #e.g., CXXFLAGS=-g -O0 for debugging
 LDFLAGS    := $(shell $(PYLON_ROOT)/bin/pylon-config --libs-rpath)
-LDLIBS     := $(shell $(PYLON_ROOT)/bin/pylon-config --libs) -lopencv_core -lopencv_highgui
+LDLIBS     := $(shell $(PYLON_ROOT)/bin/pylon-config --libs) -L/media/slash/500G/opencv/installation/OpenCV-3.4.8/lib/ -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc
 
 # Rules for building
 all: $(NAME)
--- openCVGrab.cpp
+++ openCVGrab.cpp
@@ -43,6 +43,8 @@ using namespace std;
 // Number of images to be grabbed.
 static const uint32_t c_countOfImagesToGrab = 1000;
 
+const string name_window = "Test";
+
 int main(int argc, char* argv[])
 {
     // The exit code of the sample application.
@@ -55,14 +57,15 @@ int main(int argc, char* argv[])
     try
     {
         // Create an instant camera object with the camera device found first.
-	cout << "Creating Camera..." << endl;
-	CInstantCamera camera(CTlFactory::GetInstance().CreateFirstDevice());
+		cout << "Creating Camera..." << endl;
+		CInstantCamera camera(CTlFactory::GetInstance().CreateFirstDevice());
 
         // or use a device info object to use a specific camera
-	//CDeviceInfo info;
-	//info.SetSerialNumber("21694497");
-	//CInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice(info));
-	cout << "Camera Created." << endl;
+		//CDeviceInfo info;
+		//info.SetSerialNumber("21694497");
+		//CInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice(info));
+		cout << "Camera Created." << endl;
+
         // Print the model name of the camera.
         cout << "Using device " << camera.GetDeviceInfo().GetModelName() << endl;
 
@@ -70,13 +73,13 @@ int main(int argc, char* argv[])
         // allocated for grabbing. The default value of this parameter is 10.
         camera.MaxNumBuffer = 10;
 
-	// create pylon image format converter and pylon image
-	CImageFormatConverter formatConverter;
-	formatConverter.OutputPixelFormat= PixelType_BGR8packed;
-	CPylonImage pylonImage;
+		// create pylon image format converter and pylon image
+		CImageFormatConverter formatConverter;
+		formatConverter.OutputPixelFormat= PixelType_BGR8packed;
+		CPylonImage pylonImage;
 
-	// Create an OpenCV image
-	Mat openCvImage;
+		// Create an OpenCV image
+		Mat openCvImage;
 
         // Start the grabbing of c_countOfImagesToGrab images.
         // The camera device is parameterized with a default configuration which
@@ -102,20 +105,19 @@ int main(int argc, char* argv[])
                 const uint8_t *pImageBuffer = (uint8_t *) ptrGrabResult->GetBuffer();
                 cout << "Gray value of first pixel: " << (uint32_t) pImageBuffer[0] << endl << endl;
 		
-		// Convert the grabbed buffer to pylon imag
-		formatConverter.Convert(pylonImage, ptrGrabResult);
-		// Create an OpenCV image out of pylon image
-		openCvImage= cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC3, (uint8_t *) pylonImage.GetBuffer());
+				// Convert the grabbed buffer to pylon imag
+				formatConverter.Convert(pylonImage, ptrGrabResult);
+				// Create an OpenCV image out of pylon image
+				openCvImage= cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC3, (uint8_t *) pylonImage.GetBuffer());
 
-		// Create a display window
-		namedWindow( "OpenCV Display Window", CV_WINDOW_NORMAL);//AUTOSIZE //FREERATIO
-		// Display the current image with opencv
-		imshow( "OpenCV Display Window", openCvImage);
-		// Define a timeout for customer's input in ms.
-		// '0' means indefinite, i.e. the next image will be displayed after closing the window 
-		// '1' means live stream
-		waitKey(1);
-
+				// Create a display window
+				namedWindow(name_window, CV_WINDOW_NORMAL);//AUTOSIZE //FREERATIO
+				// Display the current image with opencv
+				imshow( "OpenCV Display Window", openCvImage);
+				// Define a timeout for customer's input in ms.
+				// '0' means indefinite, i.e. the next image will be displayed after closing the window 
+				// '1' means live stream
+				waitKey(1);
             }
             else
             {
